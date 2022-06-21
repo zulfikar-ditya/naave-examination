9dbfb73ecc0e0b0f4b76ccc14644bc6e38b2fe6c
Zulfikar Ditya <zulfikarditya.work@gmail.com>
2022-06-21 19:28:09 +0700

    install databble

--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        Yajra\DataTables\DataTablesServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
