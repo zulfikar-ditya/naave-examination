a12de49c4fb21704b6e6f2f756268ec0e162f948
Zulfikar Ditya <zulfikarditya.work@gmail.com>
2022-06-21 21:30:02 +0700

    model validate

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,10 +2,53 @@
 
 namespace App\Models;
 
+use App\Http\Helpers\RandomCode;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    use HasFactory;
+    use HasFactory, RandomCode;
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = ['created_at', 'updated_at'];
+
+    public $fillable = [
+        'name',
+        // 'code',
+        // 'siup',
+        // 'npwp',
+        'contact_person',
+        'address',
+    ];
+
+    public function rules($method = 'create', $id = null)
+    {
+        $validate = [
+            'name' => 'required|string|max:255',
+            'siup' => 'required|file|max:8000',
+            'npwp' => 'required|file|max:8000',
+            'contact_person' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+        ];
+        return $validate;
+    }
+
+    public function loadModel($request, $method = 'create') {
+        foreach ($this->fillable as $key_field) {
+            foreach ($request as $key_request => $value) {
+                if ($key_field == $key_request) $this->setAttribute($key_field, $value);
+            }
+        }
+        if ($method == 'create') $this->setAttribute('code', $this->company_code($request['name']));
+    }
+
+    public function company_emails()
+    {
+        return $this->hasMany(CompanyEmail::class);
+    }
 }
--- app/Models/CompanyEmail.php
+++ app/Models/CompanyEmail.php
@@ -8,4 +8,35 @@ use Illuminate\Database\Eloquent\Model;
 class CompanyEmail extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = ['created_at', 'updated_at'];
+
+    public $fillable = [
+        'company_id',
+        'email',
+    ];
+
+    public function rules($method = 'create', $id = null)
+    {
+        $validate = [
+            'company_id' => 'required|exists:companies,id',
+        ];
+        if ($method == 'create') {
+            $unique_validate = [
+                'email' => 'required|unique:companies,email'
+            ];
+        } else {
+            $unique_validate = [
+                'email' => 'required|unique:companies,email,'.$id
+            ];
+        }
+        return array_merge($validate, $unique_validate);
+    }
+
+    public function loadModel($request) {foreach ($this->fillable as $key_field) {foreach ($request as $key_request => $value) {if ($key_field == $key_request) $this->setAttribute($key_field, $value);}}}
 }
